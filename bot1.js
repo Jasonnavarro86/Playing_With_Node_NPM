7a6c29ae563844dd2cd400bd0f100c517826236d
Jason Navarro <Jasonnavarro86@yahoo.com>
2017-09-04 23:40:43 -0400

    Sent my first tweet from Node..YESSSSSS

--- bot1.js
+++ bot1.js
@@ -6,18 +6,15 @@ var config = require('./config');
 
 var T = new Twit(config);
 
-var params = { 
-  q: 'love',
-  count: 2 
+var tweet =  { status: 'Tweeting From My BASH! #NodeWhatIMean?' }
+
+T.post('statuses/update', tweet , tweeted);
+
+function tweeted(err, data, response) {
+  console.log(data)
 }
 
-T.get('search/tweets', params, gotData);
-
-function gotData(err, data, response) {
-  
-
-  var tweets = data.statuses;
-  for (var i = 0; i < tweets.length; i++) {
-    console.log(tweets[i].text);
-  }
-};
+  // var tweets = data.statuses;
+  // for (var i = 0; i < tweets.length; i++) {
+  //   console.log(tweets[i].text);
+  // }
